5a8eceec384733dc092c1dac0d62144087fd4bbb
Patrick Hawbecker <hawbecker.3@gmail.com>
2020-07-27 10:05:49 -0600

    Adjusting setup for FINO case study period.
    GFS is best performer but domain size has increased on d01 and d02
    to allow for more CPUs in the LES cases. ∆x,y has changed from
    27 > 9 > 3 km to 25 > 5 > 1 km to allow for less nests when going
    to LES resolutions.

--- FINO_20100513to26/templates/submit_wrf_template.sh
+++ FINO_20100513to26/templates/submit_wrf_template.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #PBS -N WRFSIMNAME
 #PBS -A nwsa0002
-#PBS -l walltime=06:00:00
+#PBS -l walltime=08:00:00
 #PBS -q economy
 #PBS -j oe
 #PBS -m abe
 #PBS -M hawbecke@ucar.edu
 ### Select 2 nodes with 36 CPUs each for a total of 72 MPI processes
-#PBS -l select=4:ncpus=36:mpiprocs=36
+#PBS -l select=10:ncpus=36:mpiprocs=36
 
 date_start=`date`
 echo $date_start
