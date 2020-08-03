d42f15d7a24a7f6f70524b00f68dba7e56916d30
Patrick Hawbecker <hawbecker.3@gmail.com>
2020-08-03 12:23:55 -0600

    New 25>5>1 km setup for ERA5 w/MYJ on 5/15 to 5/17
    Templates for the new 25 km to 1 km grid spacing setup using
    MYJ PBL. The wrf_setup script calls the ERA5 reanalysis that
    will need to be downloaded manually. Period of interest is
    5/16 01Z to 05Z, thus, this simulation goes from 5/14 12Z to
    5/17/ 00Z with the first 12 hours being spinup. Simulation
    length can be shortened to end at 5/16 05Z if desired (be
    sure to modify the template file before running the setup
    script).

---
+++ FINO_20100513to26/templates/link_gfs_template.sh
@@ -0,0 +1,5 @@
+#!/bin/bash  
+ln -s /glade/collections/rda/data/ds083.2/grib2/YY1/YY1.MM1/fnl_YY1MM1DD1_00_00.grib2 
+ln -s /glade/collections/rda/data/ds083.2/grib2/YY1/YY1.MM1/fnl_YY1MM1DD1_06_00.grib2 
+ln -s /glade/collections/rda/data/ds083.2/grib2/YY1/YY1.MM1/fnl_YY1MM1DD1_12_00.grib2 
+ln -s /glade/collections/rda/data/ds083.2/grib2/YY1/YY1.MM1/fnl_YY1MM1DD1_18_00.grib2 
