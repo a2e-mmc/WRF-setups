0263f11609f010f25c102d7fff4d0a0ba30b6626
Patrick Hawbecker <hawbecker.3@gmail.com>
2020-08-03 12:44:57 -0600

    Added comments to download script

--- FINO_20100513to26/FINO_DownloadERA5_051412to051700.py
+++ FINO_20100513to26/FINO_DownloadERA5_051412to051700.py
@@ -2,23 +2,24 @@
 import pandas as pd
 from mmctools.mmctools.wrf.preprocessing import ERA5
 
+# Where you want the ERA5 data to be placed:
 icbc_dir = '/glade/scratch/hawbecke/WRF/ICBC/ERA5/'
-icbc_dir = './icbc_test/'
 
-period_s = pd.to_datetime('2010-05-12')
-period_e = pd.to_datetime('2010-05-27')
-start_hr = 12
+# Start date for download
+start_time = pd.to_datetime('2010-05-12 12:00:00')
+# End date for download
+end_time   = pd.to_datetime('2010-05-27 00:00:00')
  
+# bounds for ERA5 data:
 area = {'N':79, 'W':-63, 'S':26, 'E':77}
 
+# We want 1 hour intervals
 freq       = '1h'
 
-start_time = period_s + pd.to_timedelta(start_hr,'h')
-end_time   = period_e
-
+# Create date range from start_time to end_time
 datetimes = pd.date_range(start=start_time,
                             end=end_time,
                            freq=freq)
-
+# Download the data
 icbc = ERA5()
 icbc.download(datetimes,path=icbc_dir,bounds=area)
